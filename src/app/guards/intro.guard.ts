b8cc64ec65c71691d79f0dfd9519c659aca41365
MiguelARB1 <75139275@continental.edu.pe>
2023-07-10 21:06:51 -0500

    clase 10/jul/23

--- src/app/guards/intro.guard.ts
+++ src/app/guards/intro.guard.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage-angular';
-@Injectable({
+
+import { Router } from '@angular/router';@Injectable({
   providedIn: 'root'
 })
 export class IntroGuard implements CanActivate {
   
-  constructor(private storage:Storage){}
+  constructor(private storage: Storage, private router: Router) {}
   canActivate(){
    console.log("paso por la seguridad");
+   this.router.navigateByUrl("/intro");
     return false;
   }
   
