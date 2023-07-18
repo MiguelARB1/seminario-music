d7ace2430a1973e434dfca315e62fadb1f30767c
MiguelARB1 <75139275@continental.edu.pe>
2023-07-17 20:59:31 -0500

    clase 17-07-23

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MusicService } from '../services/music.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  artists:any;
+  localArtists:any;
 
-  constructor() {}
+  constructor(private musicService:MusicService) {}
+  ionViewDidEnter(){
+    this.musicService.getArtists().then(listArtists => {
+      this.artists = listArtists;
+      console.log("variable artists",this.artists);
+    })
 
+    this.localArtists = this.musicService.getArtistsFromJson();
+    console.log(this.localArtists.artists);
+  }
 }
